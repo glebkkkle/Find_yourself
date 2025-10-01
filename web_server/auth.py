50fcae9f7b9f4a1f38d2bad1793e31401d473bfa
gleb <klymenko8h@gmail.com>
2025-10-01 19:12:36 +0200

    added k

--- web_server/auth.py
+++ web_server/auth.py
@@ -6,7 +6,7 @@ import requests
 
 #-------------------- MAIN ------------------------
 if not firebase_admin._apps:
-    cred = credentials.Certificate(r"C:\Users\klyme\Desktop\projj\Find_yourself\hui.json")
+    cred = credentials.Certificate(r"C:\Users\klyme\Recent\hui.json.lnk")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
